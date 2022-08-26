37e0ee40f7c00d3a14f9a5ca161b68be0763e778
«Oleg <podgornov.ob@gmail.com>
2022-08-26 23:59:53 +0300

    Семинар 2

---
+++ Семинар 2_003/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 23, 34, 56, 4, 34, 234,  56, 34 };
+
+int n = array.Length;
+int find = 34;
+
+int index  = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
