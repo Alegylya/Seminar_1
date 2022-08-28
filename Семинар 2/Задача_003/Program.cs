4654edaf60b2049efb9fee9513f31ad92bc20d13
«Oleg <podgornov.ob@gmail.com>
2022-08-28 22:36:50 +0300

    Семинар 2

---
+++ Семинар 2/Задача_003/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Seminar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           System.Console.WriteLine("Введите первое число");
+           int number1 = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Введите второе число");
+           int number2 = int.Parse(Console.ReadLine());
+            if(number1 > number2)
+            {
+                if(number1 % number2  == 0)
+                {
+                    System.Console.WriteLine($"Число {number2} кратное {number1} ");
+                }
+                else
+                {
+                    System.Console.WriteLine($"Число {number2} не кратное {number1} ");
+                }
+            }
+            else
+            System.Console.WriteLine($"Число {number2} больше {number1} ");
+           
+        }
+        
+    }
+}
+   
