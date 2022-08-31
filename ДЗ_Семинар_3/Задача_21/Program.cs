def7b14e728bf07e7bc51e7da1cf80ccaae9550c
«Oleg <podgornov.ob@gmail.com>
2022-09-01 01:32:33 +0300

    Добавил ДЗ. Семинар 3

---
+++ ДЗ_Семинар_3/Задача_21/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Seminar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        System.Console.WriteLine("x1:");
+        int x1 = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("y1:");
+        int y1 = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("z1:");
+        int z1 = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("x2:");
+        int x2 = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("y2:");
+        int y2 = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("z2:");
+        int z2 = int.Parse(Console.ReadLine());
+
+
+        double result = Math.Sqrt (Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2)+ Math.Pow((z2-z1),2));
+
+        System.Console.WriteLine($"А (Длина отрезка {result:f2}");
+
+        }
+    }
+}
---
+++ ДЗ_Семинар_3/Задача_23/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Seminar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        System.Console.WriteLine("Введите число");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine($"Таблица кубов");
+
+        for(int i = 1; i <= number; i++)
+        {
+            System.Console.WriteLine(Math.Pow(i,3));
+        }
+        }
+    }
+}
