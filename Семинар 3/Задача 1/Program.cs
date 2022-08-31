b0fbc632e563086fc9e81e11d85497ad9c0aef6e
«Oleg <podgornov.ob@gmail.com>
2022-09-01 00:58:43 +0300

    Семинар 3

---
+++ Семинар 3/Задача 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Seminar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           System.Console.WriteLine("Введите четверть");
+           int quaternumber = int.Parse(Console.ReadLine());
+
+            if (quaternumber == 1)
+            {
+                System.Console.WriteLine("x > 0, y < 0");
+            }
+            else if (quaternumber == 2)
+            {
+                System.Console.WriteLine("x < 0, y > 0");
+            }
+           else if (quaternumber == 3)
+            {
+                System.Console.WriteLine("x < 0, y < 0");
+            }
+           else if (quaternumber == 4)
+            {
+                System.Console.WriteLine("x > 0, y < 0");
+            }
+            else
+            {
+                System.Console.WriteLine($"{quaternumber} нет такой четверти");
+            }
+        }
+    }
+}
---
+++ Семинар 3/Задача_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Seminar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        System.Console.WriteLine("x1:");
+        int x1 = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("y1:");
+        int y1 = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("x2:");
+        int x2 = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("y2:");
+        int y2 = int.Parse(Console.ReadLine());
+
+        double result = Math.Sqrt (Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2));
+
+        System.Console.WriteLine($"А ({x1},{y1}; B {x2},{y2}){result:f2}");
+
+        }
+    }
+}
